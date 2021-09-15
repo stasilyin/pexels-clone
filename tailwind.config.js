454e9b4382335231c61084442492adb7f0b33e22
stasilyin <stasilyin.by@gmail.com>
2021-09-15 09:54:58 +0300

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    darkMode: 'class',
+    purge: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.js', './src/**/*.ts', './src/**/*.tsx'],
+    theme: {
+        extend: {
+            screens: {
+                '3xl': '2048px',
+                '2-3xl': '1900px',
+                '2xl': '1920px',
+                '1h-signUp': { raw: '(min-height: 927px)' },
+                '1h-signIn': { raw: '(min-height: 892px)' },
+            },
+            colors: {
+                gray: {
+                    950: 'rgba(152, 152, 152, 0.4)',
+                },
+            },
+            fontFamily: {
+                roboto: ['Roboto', 'Inter', 'system-ui', '-apple-system'],
+            },
+        },
+    },
+    variants: {
+        extend: {
+            boxShadow: ['dark'],
+        },
+    },
+}
